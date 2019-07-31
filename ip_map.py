cd183d2222f63bafeb1d31e49826e1fa89c35fc3
alf808 <alf@akademe.net>
2019-07-31 15:12:22 -0400

    added error handling to ip_map.py

--- ip_map.py
+++ ip_map.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-
+'''This tool will return the corresponding IP address of any domain entered.
+Usage: ./ip_map.py <hostname>
+Sample: ./ip_map.py devleague.com
+'''
 import socket
 import argparse
-
-print("convert host to IP")
-print("example: google.com")
+import sys
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('host', help="domain name here")
+parser.add_argument('host', help="mandatory host name")
 args = parser.parse_args()
 
-dom = args.host
+temp_host = sys.argv[1]
 
-print(socket.gethostbyname(dom))
+try:
+    temp_ip = socket.gethostbyname(args.host)
+except OSError:
+    print("Unable to obtain IP with that hostname")
+else:
+    print(f"The IP address of {temp_host} is: {temp_ip}")
