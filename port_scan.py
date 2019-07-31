a448b6dac0d6a814de823e8dce08db5e97cec97d
alf808 <alf@akademe.net>
2019-07-31 17:56:27 -0400

    added scan port feature

--- port_scan.py
+++ port_scan.py
@@ -13,13 +13,28 @@ import socket
 import argparse
 import sys
 
-def scan_ip(ip, start_port, end_port):
-    print(f"begin scan {ip} from {start_port} to {end_port}")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+open_ports = []
+
+def scan_ip():
+    print(f"begin scan {target_ip} from {port_start} to {port_end}")
+    for port in range(port_start, port_end+1):
+        scan_port(port)
 
 
-def range_type(astr, min=0, max=65535):
-    '''verify if port is within range of 0 to 65535'''
-    value = int(astr)
+def scan_port(port):
+    try:
+        status = sock.connect_ex((target_ip, port))
+        if status == 0:
+            open_ports.append(port)
+        # return
+    except:
+        print("something else happened that cannot be handled")
+
+
+def port_range(num, min=0, max=65535):
+    '''verify if port is within range from 0 to 65535'''
+    value = int(num)
     if min<= value <= max:
         return value
     else:
@@ -34,28 +49,34 @@ parser = argparse.ArgumentParser(
     description="command line tool to scan ports of an IP or host"
 )
 
+# ensures that IP and HOST are mutually exclusive
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-ip', help="IP must be valid")
 group.add_argument('-host', help="Tool will attempt host name")
 
-parser.add_argument('-start', type=range_type, metavar="[0-65535]", help="starting port to scan")
-parser.add_argument('-end', type=range_type, metavar='[0-65535]', help="last port to scan")
+parser.add_argument('-start', type=port_range, metavar="[0-65535]", help="starting port to scan")
+parser.add_argument('-end', type=port_range, metavar='[0-65535]', help="last port to scan")
 args = parser.parse_args()
 
 try:
     if sys.argv[1]=='-ip' and args.ip:
         socket.inet_aton(args.ip)
         host_data = socket.gethostbyaddr(args.ip)
-        ip_data = host_data[2][0]
+        target_ip = host_data[2][0]
     elif sys.argv[1]=='-host' and args.host:
-        ip_data = socket.gethostbyname(args.host)
+        target_ip = socket.gethostbyname(args.host)
 except OSError:
     print("Unable to obtain information with that IP or host")
 else:
     if sys.argv[1]=='-ip':
-        print(f"The hostname is: {host_data[0]} for IP: {ip_data}")
+        print(f"The hostname is: {host_data[0]} for IP: {target_ip}")
     elif sys.argv[1]=='-host':
-        print(f"The IP address of {sys.argv[2]} is: {ip_data}")
-    scan_ip(ip_data, args.start, args.end)
-
+        print(f"The IP address of {sys.argv[2]} is: {target_ip}")
+    port_start = args.start
+    port_end = args.end
+    scan_ip()
+    if len(open_ports) == 0:
+        print("no open ports")
+    else:
+        print(open_ports)
 
