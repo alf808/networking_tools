a59114316e5d909ac84d1e4c6a2bc3bd042fee0c
alf808 <alf@akademe.net>
2019-07-31 14:33:00 -0400

    updated sys info with error handling

--- sys_info.py
+++ sys_info.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-
+'''This tool will grab the hostname of the target machine.
+Usage: ./sys_info.py <IP>
+Sample: ./sys_info.py 127.0.0.1
+'''
 import socket
 import argparse
 
-print("convert IP to host")
-print("example: 127.0.0.1")
+parser = argparse.ArgumentParser(
+    prog="Network Survival Kit",
+    description="command line tool to convert IP to host"
+    )
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('ip', help="domain name here")
+parser.add_argument('ip', help="mandatory IP address")
 args = parser.parse_args()
 
-ip_data = args.ip
-
-print(socket.gethostbyaddr(ip_data))
+try:
+    socket.inet_aton(args.ip)
+except OSError:
+    print("valid IP please")
+else:
+    print(f"The hostname is: {socket.gethostbyaddr(args.ip)[0]} for IP: {socket.gethostbyaddr(args.ip)[2][0]}")
