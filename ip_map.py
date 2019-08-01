949a39b502191a57b4cc0c229f567166fa312459
alf808 <alf@akademe.net>
2019-08-01 17:49:42 -0400

    implemented report tool for some modules

--- ip_map.py
+++ ip_map.py
@@ -5,33 +5,40 @@ Sample: ./ip_map.py devleague.com
 '''
 import socket
 import argparse
-import sys
-from datetime import datetime
+import net_report
 
-if len(sys.argv) > 2:
-    sys.exit("too many or not enough arguments")
 
-start = datetime.now()
-output = ""
+def getinfo():
+    net_report.scan_begin()
+    output = ""
 
-parser = argparse.ArgumentParser(
-    prog="Network Survival Kit",
-    description="command line tool to obtain IP from host"
-)
+    parser = argparse.ArgumentParser(
+        prog="Network Survival Kit",
+        description="command line tool to obtain IP from host"
+    )
 
-parser.add_argument('host', help="mandatory host name")
-args = parser.parse_args()
+    parser.add_argument('host', help="mandatory host name")
+    args = parser.parse_args()
 
-temp_host = sys.argv[1]
+    temp_host = sys.argv[1]
 
-try:
-    temp_ip = socket.gethostbyname(args.host)
-except OSError:
-    print("Unable to obtain IP with that hostname")
-else:
-    output += f"The IP address of {temp_host} is: {temp_ip}"
-finally:
-    end = datetime.now()
-    duration = end - start
-    output += f"\nduration of task: {duration}"
-    print(output)
+    try:
+        temp_ip = socket.gethostbyname(args.host)
+    except OSError:
+        print("Unable to obtain IP with that hostname")
+    else:
+        output += f"The IP address of {temp_host} is: {temp_ip}"
+    finally:
+        net_report.scan_end()
+        duration = net_report.show_duration()
+        output += f"\nduration of task: {duration}"
+        print(output)
+        net_report.collect(output)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 2:
+        sys.exit("too many or not enough arguments")
+
+    getinfo()
--- sys_info.py
+++ sys_info.py
@@ -5,32 +5,41 @@ Sample: ./sys_info.py 127.0.0.1
 '''
 import socket
 import argparse
-from datetime import datetime
-import sys
+import net_report
 
-if len(sys.argv) > 2:
-    sys.exit("too many or not enough arguments")
 
-start = datetime.now()
-output = ""
+def getinfo():
+    net_report.scan_begin()
+    output = ""
 
-parser = argparse.ArgumentParser(
-    prog="Network Survival Kit",
-    description="command line tool to convert IP to host"
-    )
+    parser = argparse.ArgumentParser(
+        prog="Network Survival Kit",
+        description="command line tool to convert IP to host"
+        )
 
-parser.add_argument('ip', help="mandatory IP address")
-args = parser.parse_args()
+    parser.add_argument('ip', help="mandatory IP address")
+    args = parser.parse_args()
 
-try:
-    socket.inet_aton(args.ip)
-    ip_info = socket.gethostbyaddr(args.ip)
-except OSError:
-    print("valid IP please")
-else:
-    output += f"The hostname is: {ip_info[0]} for IP: {ip_info[2][0]}"
-finally:
-    end = datetime.now()
-    duration = end - start
-    output += f"\nduration of task: {duration}"
-    print(output)
+    try:
+        # inet_aton tests whether IP is valid otherwise oserror
+        socket.inet_aton(args.ip)
+        ip_info = socket.gethostbyaddr(args.ip)
+    except OSError:
+        print("invalid or non-existent IP")
+    else:
+        output += f"The hostname is: {ip_info[0]} for IP: {ip_info[2][0]}"
+    finally:
+        net_report.scan_end()
+        duration = net_report.show_duration()
+        output += f"\nduration of task: {duration}"
+        print(output)
+        net_report.collect(output)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 2:
+        sys.exit("too many or not enough arguments")
+
+    getinfo()
