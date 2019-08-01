a5f9a2b63a53143478020c96ae271bd04842a6fe
alf808 <alf@akademe.net>
2019-08-01 17:55:05 -0400

    reorganized structure of port_scan

--- port_scan.py
+++ port_scan.py
@@ -11,26 +11,23 @@ Sample:
 '''
 import socket
 import argparse
-import sys
-from datetime import datetime
+import net_report
 
-if len(sys.argv) < 2 or len(sys.argv) > 7:
-    sys.exit("too many or not enough arguments")
-
-start = datetime.now()
-output = ""
+# global variables
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 open_ports = []
+target_ip = ""
 
 
-def scan_ip():
+def _scan_ip(port_start, port_end):
     '''Begin of scan with target IP and port range'''
+    global target_ip
     print(f"scanning {target_ip} ports {port_start} to {port_end}")
     for port in range(port_start, port_end+1):
-        scan_port(port)
+        _scan_port(port)
 
 
-def scan_port(port):
+def _scan_port(port):
     '''Scanning each port'''
     try:
         status = sock.connect_ex((target_ip, port))
@@ -49,40 +46,55 @@ def port_range(num, min=0, max=65535):
         raise argparse.ArgumentTypeError('value not in range %s-%s'%(min,max))
 
 
-parser = argparse.ArgumentParser(
-    prog="Network Survival Kit",
-    description="command line tool to scan ports of an IP or host"
-)
+def getinfo():
+    net_report.scan_begin()
+    output = ""
+    global target_ip
 
-# ensures that IP and HOST are mutually exclusive
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-ip', help="IP must be valid")
-group.add_argument('-host', help="Tool will attempt host name")
+    parser = argparse.ArgumentParser(
+        prog="Network Survival Kit",
+        description="command line tool to scan ports of an IP or host"
+    )
 
-parser.add_argument('-start', type=port_range, metavar="[0-65535]", help="starting port to scan")
-parser.add_argument('-end', type=port_range, metavar='[0-65535]', help="last port to scan")
-args = parser.parse_args()
+    # ensures that IP and HOST are mutually exclusive
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-ip', help="IP must be valid")
+    group.add_argument('-host', help="Tool will attempt host name")
 
-try:
-    if sys.argv[1]=='-ip' and args.ip:
-        socket.inet_aton(args.ip)
-        host_data = socket.gethostbyaddr(args.ip)
-        target_ip = host_data[2][0]
-    elif sys.argv[1]=='-host' and args.host:
-        target_ip = socket.gethostbyname(args.host)
-except OSError:
-    print(f"Unable to obtain information for {sys.argv[2]}")
-else:
-    port_start = args.start
-    port_end = args.end
-    scan_ip()
-    if len(open_ports) == 0:
-        output += f"{sys.argv[2]} no open ports"
+    parser.add_argument('-start', type=port_range, metavar="[0-65535]", help="starting port to scan")
+    parser.add_argument('-end', type=port_range, metavar='[0-65535]', help="last port to scan")
+    args = parser.parse_args()
+
+    try:
+        if sys.argv[1]=='-ip' and args.ip:
+            socket.inet_aton(args.ip)
+            host_data = socket.gethostbyaddr(args.ip)
+            target_ip = host_data[2][0]
+        elif sys.argv[1]=='-host' and args.host:
+            target_ip = socket.gethostbyname(args.host)
+    except OSError:
+        print(f"Unable to obtain information for {sys.argv[2]}")
     else:
-        for p in open_ports:
-            output += f"Port {p}: OPEN\n"
-finally:
-    end = datetime.now()
-    duration = end - start
-    output += f"\nduration of task: {duration}"
-    print(output)
+        port_start = args.start
+        port_end = args.end
+        _scan_ip(port_start, port_end)
+        if len(open_ports) == 0:
+            output += f"{sys.argv[2]} no open ports"
+        else:
+            for p in open_ports:
+                output += f"Port {p}: OPEN\n"
+    finally:
+        net_report.scan_end()
+        duration = net_report.show_duration()
+        output += f"\nduration of task: {duration}"
+        print(output)
+        net_report.collect(output)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2 or len(sys.argv) > 7:
+        sys.exit("too many or not enough arguments")
+
+    getinfo()
