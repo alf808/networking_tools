33f4fd6a5095b7586827725c9fd2d47929a8f749
alf808 <alf@akademe.net>
2019-07-31 23:11:20 -0400

    fixed mac_lookup

--- mac_lookup.py
+++ mac_lookup.py
@@ -3,27 +3,38 @@
 Usage: ./mac_lookup.py <mac_address>
 Sample: ./mac_lookup.py 00:11:22:33:44:55
 '''
-import urllib
+import urllib.request
 import json
-import codecs
 import argparse
+import sys
+
+if len(sys.argv) > 2:
+    sys.exit("too many or not enough arguments")
 
 parser = argparse.ArgumentParser(
     prog="Network Survival Kit",
-    description="command line tool to convert IP to host"
+    description="command line tool to obtain MAC address info"
     )
 
-parser.add_argument('url', help="mandatory url")
+parser.add_argument('mac', help="mandatory MAC")
 args = parser.parse_args()
 
 mac_api_url = "http://macvendors.co/api/"
 
 try:
-    req = urllib.request(mac_api_url + args.url, headers={'User-Agent' : "API Browser"}) 
-    response = urllib.urlopen(request)
-    reader = codecs.getreader("utf-8")
-    obj = json.load(reader(response))
+    url_full = mac_api_url + args.mac + '/json'
+    with urllib.request.urlopen(url_full) as response:
+        result = response.read().decode('utf-8')
+    obj = json.loads(result)
 except:
-    print("oops")
+    print("dunno")
 else:
-    print(obj)
+    try:
+        company = obj['result']['company']
+        address = obj['result']['address']
+    except:
+        print("unrecognizable MAC address")
+    else:
+        output = f"company: {company}\naddress: {address}"
+        print(output)
+    
