bbb5390ccc3a4a9bb147502c40da965409aa72b6
alf808 <alf@akademe.net>
2019-08-02 23:20:29 -0400

    rewrote sys_info to be importable also

---
+++ imported_tools.py
@@ -0,0 +1,4 @@
+#import ip_map
+import sys_info
+
+print(sys_info.getinfo('8.8.8.8'))
--- sys_info.py
+++ sys_info.py
@@ -4,26 +4,18 @@ Usage: ./sys_info.py <IP>
 Sample: ./sys_info.py 127.0.0.1
 '''
 import socket
+import sys
 import argparse
 import net_report
 
 
-def getinfo():
-    net_report.scan_begin()
+def getinfo(ip):
     output = ""
-
-    parser = argparse.ArgumentParser(
-        prog="Network Survival Kit",
-        description="command line tool to convert IP to host"
-        )
-
-    parser.add_argument('ip', help="mandatory IP address")
-    args = parser.parse_args()
-
+    net_report.scan_begin()
     try:
         # inet_aton tests whether IP is valid otherwise oserror
-        socket.inet_aton(args.ip)
-        ip_info = socket.gethostbyaddr(args.ip)
+        socket.inet_aton(ip)
+        ip_info = socket.gethostbyaddr(ip)
     except OSError:
         print("invalid or non-existent IP")
     else:
@@ -32,14 +24,21 @@ def getinfo():
         net_report.scan_end()
         duration = net_report.show_duration()
         output += f"\nduration of task: {duration}"
-        print(output)
         net_report.collect(output)
+        return output
 
 
 if __name__ == "__main__":
-    import sys
 
     if len(sys.argv) > 2:
         sys.exit("too many or not enough arguments")
 
-    getinfo()
+    parser = argparse.ArgumentParser(
+        prog="Network Survival Kit",
+        description="command line tool to convert IP to host"
+        )
+
+    parser.add_argument('ip', help="mandatory IP address")
+    args = parser.parse_args()
+    ip_data = args.ip
+    print(getinfo(ip_data))
