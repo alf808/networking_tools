52c2989cdd15b21667941a5b35baafaa08e475da
alf808 <alf@akademe.net>
2019-08-03 15:12:49 -0400

    rewrote client_browse to be importable

--- client_browse.py
+++ client_browse.py
@@ -5,50 +5,47 @@ Sample: ./client_browse.py https://github.com
 '''
 import socket
 import argparse
+import sys
 import urllib.request
 import net_report
 
 
-net_report.scan_begin()
-output = ""
-
-parser = argparse.ArgumentParser(
-    prog="Network Survival Kit",
-    description="command line tool which runs as a client browser"
-    )
-
-parser.add_argument('url', help="mandatory URL")
-args = parser.parse_args()
-
-try:
-    with urllib.request.urlopen(args.url) as response:
-        html = response.read(1000).decode('utf-8')
-        url = response.geturl()
-        code = response.getcode()
-        info = response.info()
-except urllib.error.URLError:
-    print("URL error. Perhaps the URL is invalid; or URL has no http info")
-except ValueError:
-    print(r"Perhaps prefixation of 'http://' or 'https://' might help")
-else: 
-    output += f'''
-URL: {url}
-Status-code: {code}
-{info}
-{html}
-    '''
-finally:
-    net_report.scan_end()
-    duration = net_report.show_duration()
-    output += f"\nduration of task: {duration}"
-    print(output)
-    net_report.collect(output)
+def getinfo(ur):
+    net_report.scan_begin()
+    output = ""
+    try:
+        with urllib.request.urlopen(ur) as response:
+            html = response.read(1000).decode('utf-8')
+            url = response.geturl()
+            code = response.getcode()
+            info = response.info()
+    except urllib.error.URLError:
+        print("URL error. Perhaps the URL is invalid; or URL has no http info")
+    except ValueError:
+        print(r"Perhaps prefixation of 'http://' or 'https://' might help")
+    else: 
+        output += f'''
+        URL: {url}
+        Status-code: {code}
+        {info}
+        {html}
+        '''
+    finally:
+        net_report.scan_end()
+        duration = net_report.show_duration()
+        output += f"\nduration of task: {duration}"
+        net_report.collect(output)
+        return output
 
 
 if __name__ == "__main__":
-    import sys
 
     if len(sys.argv) > 2:
         sys.exit("too many or not enough arguments")
 
-    getinfo()
+    parser = argparse.ArgumentParser(description="command line tool which runs as a client browser")
+
+    parser.add_argument('url', help="mandatory URL")
+    args = parser.parse_args()
+    
+    print(getinfo(args.url))
